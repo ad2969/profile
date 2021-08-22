2f20e52b8dd8cb3445ab359654d7fdd03254fa46
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-22 06:52:13 -0700

    add details to hero

--- src/routes/Home/Hero.tsx
+++ src/routes/Home/Hero.tsx
@@ -6,7 +6,30 @@ import CustomIcon from "../../components/media/customImageIcon";
 import akpsiLogo from "../../assets/images/logos/akpsi-logo.png";
 import biztechLogo from "../../assets/images/logos/biztech-logo.png";
 import semiosLogo from "../../assets/images/logos/semios-logo.png";
+import skyrocketLogo from "../../assets/images/logos/skyrocket-logo.png";
 import face from "../../assets/images/face.png";
+
+interface LeenkProps {
+    to?: string;
+    href?: string;
+    children: React.ReactNode;
+}
+
+const Leenk: React.FunctionComponent<LeenkProps> = ({ href = "", to = "/", children }) => {
+    if (href) {
+        return (
+            <a className="t--default button--darken" href={href} >
+                {children}
+            </a>
+        );
+    } else {
+        return (
+            <Link className="t--default button--darken" to={to}>
+                {children}
+            </Link>
+        );
+    }
+};
 
 const Hero: React.FunctionComponent = () => {
     return (
@@ -16,15 +39,16 @@ const Hero: React.FunctionComponent = () => {
             </div>
             <ul className="hero__text">
                 <div className="hero__text-title">Hello, I&apos;m Clarence. I like to build things.</div>
-                <li>Currently based in <Link className="t--default button--darken" to="/">&#128205; Vancouver, Canada</Link></li>
-                <li>Studying Electrical Engineering at the <Link className="t--default button--darken" to="/">&#127891; University of British Columbia</Link></li>
+                <li>Currently based in <Leenk to="/">&#128205;Vancouver, Canada</Leenk></li>
+                <li>Studying Electrical Engineering at the <Leenk to="/">&#127891;University of British Columbia</Leenk></li>
                 <li>Part of &nbsp;
-                    <Link className="t--default button--darken" to="/"><CustomIcon img={biztechLogo} /> UBC Biztech</Link>,&nbsp;
-                    <Link className="t--default button--darken" to="/"><CustomIcon img={akpsiLogo} /> Alpha Kappa Psi</Link> Business Fraternity
+                    <Leenk to="/"><CustomIcon img={biztechLogo} /> UBC Biztech</Leenk>,&nbsp;
+                    <Leenk to="/"><CustomIcon img={akpsiLogo} /> Alpha Kappa Psi</Leenk> Business Fraternity
                 </li>
                 <br />
-                <li>Looking for  <Link className="t--default button--darken" to="/">&#128084; Co-op Internships</Link> for Summer 2022</li>
-                <li>Previously a Full-Stack Javascript Developer Co-op at <Link className="t--default button--darken" to="/"><CustomIcon img={semiosLogo} /> Semios</Link></li>
+                <li>Doing work at <Leenk href="https://skyrocket.is/"><CustomIcon img={skyrocketLogo} /> Skyrocket Digital</Leenk></li>
+                <li>Looking for <Leenk to="/">&#128084;Co-op Internships</Leenk> for Summer 2022</li>
+                <li>Previously a Full-Stack Javascript Developer Co-op at <Leenk to="/"><CustomIcon img={semiosLogo} /> Semios</Leenk></li>
             </ul>
         </div>);
 };
