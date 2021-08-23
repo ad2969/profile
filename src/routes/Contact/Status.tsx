1503573b573c9547273d6fd77d03f1a2edb03e53
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-23 16:51:12 -0700

    add contact silhouette and status flag

--- src/routes/Contact/Status.tsx
+++ src/routes/Contact/Status.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./styles.scss";
 
-const ContactStatus: React.FunctionComponent = () => {
-    const status = false;
+interface Props {
+    status: boolean;
+    setStatus: (status: boolean) => void;
+    doScroll: boolean;
+}
+
+const ContactStatus: React.FunctionComponent<Props> = ({ status, setStatus, doScroll }) => {
     const statusDiv = status
         ? <span className="active button">ONLINE</span>
         : <span className="inactive button">AFK</span>;
 
+    useEffect(() => {
+        if (!doScroll) return;
+        const scrollNode = document.getElementById("contact-status");
+        if (scrollNode) scrollNode.scrollIntoView();
+    }, [doScroll]);
+
     return (
-        <div className="contact-status">
+        <div className="contact-status" id="contact-status">
             <div className="contact-status__location">&#128205;Vancouver, Canada</div>
-            <div className="contact-status__text">I am currently {statusDiv}</div>
+            <div className="contact-status__text" onClick={() => { setStatus(!status); }}>Currently {statusDiv}</div>
         </div>
     );
 };
--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import Layout from "../../components/layouts/magazine";
 import City from "./city";
@@ -8,14 +9,20 @@ import ContactStatus from "./Status";
 import "./styles.scss";
 
 const Contact: React.FunctionComponent = () => {
+    const history = useHistory();
+    const doScroll = history.location.search.includes("status");
+
+    // TODO: Get actual db data
+    const [status, setStatus] = useState(false);
+
     return (
         <Layout className="Contact">
             <div className="city">
-                <City />
+                <City status={status}/>
             </div>
             <ContactInfo />
             <div className="contact-divider"></div>
-            <ContactStatus />
+            <ContactStatus status={status} setStatus={setStatus} doScroll={doScroll}/>
         </Layout>
     );
 };
