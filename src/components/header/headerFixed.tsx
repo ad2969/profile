f00ee36cd976a5439fb309437059b142dca31bb4
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2021-08-23 18:12:36 -0700

    change header and menu button

--- src/components/header/headerFixed.tsx
+++ src/components/header/headerFixed.tsx
@@ -1,13 +1,36 @@
 import React, { useLayoutEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+import MenuButton from "../../assets/svg/menuButton";
+
+import COLORS from "../../styles/themes/_default.module.scss";
 import BREAKPOINTS from "../../styles/mixins/_breakpoints.module.scss";
 import "./styles.scss";
 
-const Header: React.FunctionComponent = () => {
+interface Props {
+    variation?: string;
+}
+
+const LINK_VARIATIONS = {
+    home: [
+        {
+            text: "at a glance",
+            url: "/"
+        },
+        {
+            text: "contact",
+            url: "/contact"
+        }
+    ]
+};
+
+const Header: React.FunctionComponent<Props> = ({ variation = "home" }) => {
     const [doShowAll, setDoShowAll] = useState(false);
 
     useLayoutEffect(() => {
+        // dont show any of the stuff if variation = 0
+        if (variation === "none") return;
+
         const updateHeader = () => {
             const breakpoint = parseInt(BREAKPOINTS.tablet.replace("px", ""));
             if (window.innerWidth < breakpoint) setDoShowAll(false);
@@ -20,25 +43,17 @@ const Header: React.FunctionComponent = () => {
 
     return (
         <div className="Header-fixed flex-center">
-            <div className="header__links">
-                {doShowAll && <span className="header__link button--scale">
-                    <NavLink
-                        className="t--default t--lowercase t--unselectable"
-                        activeClassName="selected"
-                        to="/" exact
-                    >at a glance</NavLink>
-                </span>}
-                {doShowAll && <span className="header__link button--scale">
-                    <NavLink
-                        className="t--default t--lowercase t--unselectable"
-                        activeClassName="selected"
-                        to="/contact" exact
-                    >contact</NavLink>
-                </span>}
-                <span className="header__link button--scale">
-                    <span className="t--default t--lowercase t--unselectable">menu</span>
-                </span>
-            </div>
+            {doShowAll && LINK_VARIATIONS[variation] && LINK_VARIATIONS[variation].map((link) => (
+                <NavLink
+                    key={link.url}
+                    className="header__link button--scale t--default t--lowercase t--unselectable"
+                    activeClassName="current"
+                    to={link.url} exact
+                >{link.text}</NavLink>
+            ))}
+            <span className="header__link header__link-menu button--scale t--default t--lowercase t--unselectable">
+                <MenuButton color={COLORS.primary} active={false}/>
+            </span>
         </div>
     );
 };
--- src/components/layouts/single.tsx
+++ src/components/layouts/single.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import HeaderFixed from "../header/headerFixed";
 import HeaderLogo from "../header/headerLogo";
+
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
+    variation?: string;
 }
 
-const SinglePageLayout: React.FunctionComponent<Props> = ({ children, className = "", ...rest }) => {
+const SinglePageLayout: React.FunctionComponent<Props> = ({ children, variation, className = "", ...rest }) => {
     return (
         <div className={"Page--single " + className} {...rest}>
-            <HeaderFixed />
+            <HeaderFixed variation={variation} />
             <div className="Content">
                 <HeaderLogo />
                 {children}
